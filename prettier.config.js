e73572a1db4a8bf1e55e640a83de594400649064
KenLee-Meek <kenlee@meek.co.kr>
2020-11-21 23:51:28 +0900

    nextjs with tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	semi: true,
+	singleQuote: false,
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: true,
+	trailingComma: "none",
+	bracketSpacing: true
+};
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<div className="w-screen h-screen flex justify-center items-center">
+			<h1 className="text-5xl font-bold text-green-600">with tailwindcss</h1>
+		</div>
+	);
+}
